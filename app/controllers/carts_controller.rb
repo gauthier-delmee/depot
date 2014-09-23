dd1c66d1a8aec2d926395bf0fb4d9e006af005ec
Gauthier Delmee <gauthier.delmee@cyarasolutions.com>
2014-09-23 16:56:19 +1000

    updated the cart functionality

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -55,8 +55,10 @@ class CartsController < ApplicationController
   # DELETE /carts/1.json
   def destroy
     @cart.destroy
+    session[:cart_id]
+    
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to store_url, notice: 'Your cart is currently empty.' }
       format.json { head :no_content }
     end
   end
@@ -64,7 +66,17 @@ class CartsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
-      @cart = Cart.find(params[:id])
+      begin
+        @cart = Cart.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        logger.error "Attempt to access invalid cart #{params[:id]}"
+        redirect_to store_url, :notice => 'Invalid cart'
+      #else
+      #  respond_to do |format|
+      #    format.html
+      #    format.json { render :json => @cart }
+      #  end
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
